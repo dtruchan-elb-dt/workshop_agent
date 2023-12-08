acd1cd4d6c9b7f2713a6fb9f722c3d44c177bce3
dtruchan-elb-dt <dimitrios.truchan@bayer.com>
2023-12-08 20:00:27 +0100

    Add real  memory to the agent for multi turn conversations

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from langchain.prompts import (
 )
 from langchain.schema import SystemMessage
 from langchain.agents import OpenAIFunctionsAgent, AgentExecutor
+from langchain.memory import ConversationBufferMemory
 from dotenv import load_dotenv
 
 from tools.sql import run_query_tool, list_tables, describe_tables_tool
@@ -18,24 +19,24 @@ chat = ChatOpenAI()
 tables = list_tables()
 prompt = ChatPromptTemplate(
     messages=[
-        # System Message will tell gpt which tables are available
         SystemMessage(content=(
-        "You are an AI that has access to a SQLite database.\n"
-        f"The database has tables of: {tables}\n"
-        "Do not make any assumptions about what tables exist"
-        "or what columns exist. Instead, use the 'describe_tables' function"
+            "You are an AI that has access to a SQLite database.\n"
+            f"The database has tables of: {tables}\n"
+            "Do not make any assumptions about what tables exist "
+            "or what columns exist. Instead, use the 'describe_tables' function"
         )),
-        # Human ´input´
+        MessagesPlaceholder(variable_name="chat_history"),
         HumanMessagePromptTemplate.from_template("{input}"),
-        # Create simple memory for the agent
         MessagesPlaceholder(variable_name="agent_scratchpad")
     ]
 )
 
-tools = [   run_query_tool, 
-            describe_tables_tool,
-            write_report_tool
-            ]
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+tools = [
+    run_query_tool,
+    describe_tables_tool,
+    write_report_tool
+]
 
 agent = OpenAIFunctionsAgent(
     llm=chat,
@@ -46,13 +47,14 @@ agent = OpenAIFunctionsAgent(
 agent_executor = AgentExecutor(
     agent=agent,
     verbose=True,
-    tools=tools
+    tools=tools,
+    memory=memory
 )
 
 agent_executor(
-    "How many orders are there? Write the results to a report file."
-    )
+    "How many orders are there? Write the result to an html report."
+)
 
 agent_executor(
-    "Repeat exact same process for users."
-    )
+    "Repeat the exact same process for users."
+)
